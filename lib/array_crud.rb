32527234e1f71b8586f9f5dbfac2b0babbe1495d
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-14 13:20:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   famous_cats = ["steve","stu","sam"]
   famous_cats.push("Joe")
+  puts 
 end
 
 def add_element_to_start_of_array(array, element)
